b54e25c9dae95711eb9ae22af9d3ee6a03df4d68
Luca Leonardo Scorcia <lucaleonardo.scorcia@mise.gov.it>
2023-01-23 09:45:36 +0100

    Add project files.

---
+++ DevExtremeAspNetCoreApp2/Pages/Viewer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevExtremeAspNetCoreApp2.Pages
+{
+    public class ViewerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ DevExtremeAspNetCoreApp2/Program.cs
@@ -0,0 +1,31 @@
+using DevExpress.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services
+    .AddRazorPages()
+    .AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
+
+builder.Services.AddDevExpressControls();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseDevExpressControls();
+
+app.MapControllers();
+app.MapDefaultControllerRoute();
+app.MapRazorPages();
+
+app.Run();
